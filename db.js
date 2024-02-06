cb029f50960b589845d3ad83bb871a204b14ab00
zumbost1k <matusevich.mi@gmail.com>
2024-02-06 12:37:54 +0300

    fix db

--- db.js
+++ db.js
@@ -1,12 +1,7 @@
 const { Sequelize } = require('sequelize');
 
-module.exports = new Sequelize(
-  process.env.DB_NAME, //bd name
-  process.env.DB_USER, //name of the bd user
-  process.env.DB_PASSWORD, //bd password
-  {
-    dialect: 'postgres',
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-  }
-);
+module.exports = new Sequelize(process.env.DB_URL, {
+  define: {
+    timestamps: false,
+  },
+});
