362f342d852203350943fd1a12c10a8495543fd6
Миша Матусевич <matusevich.mi@gmail.com>
2025-02-25 17:59:47 +0300

    actual portfolio

--- db.js
+++ db.js
@@ -2,6 +2,23 @@ const { Sequelize } = require('sequelize');
 
 module.exports = new Sequelize(process.env.DB_URL, {
   define: {
-    timestamps: false,
-  },
+    timestamps: false
+  }
 });
+
+// code for local connection
+// const { Sequelize } = require('sequelize');
+
+// module.exports = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
+//   {
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     dialect: 'postgres',
+//     define: {
+//       timestamps: false
+//     }
+//   }
+// );
